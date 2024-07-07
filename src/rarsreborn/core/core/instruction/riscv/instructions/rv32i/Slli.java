552f63fc371fc8577475c21e6026a22e9e70d4f8
Elina Kuzmichyova <e.kuzmichyova@innopolis.university>
2024-07-07 16:24:57 +0000

    feat: add andi, ori, slli, srli instructions

---
+++ src/rarsreborn/core/core/instruction/riscv/instructions/rv32i/Slli.java
@@ -0,0 +1,69 @@
+package rarsreborn.core.core.instruction.riscv.instructions.rv32i;
+
+import rarsreborn.core.compilation.compiler.riscv.InstructionRegexParserRegisterBase;
+import rarsreborn.core.core.instruction.ILinkableInstruction;
+import rarsreborn.core.core.instruction.riscv.RiscV32InstructionHandler;
+import rarsreborn.core.core.instruction.riscv.formats.InstructionI;
+import rarsreborn.core.core.program.LinkRequest;
+import rarsreborn.core.core.register.IRegisterFile;
+import rarsreborn.core.core.register.Register32;
+import rarsreborn.core.exceptions.compilation.CompilationException;
+import rarsreborn.core.exceptions.compilation.ImmediateTooLargeException;
+import rarsreborn.core.exceptions.compilation.UnknownRegisterException;
+
+public class Slli extends InstructionI implements ILinkableInstruction {
+    public static final String NAME = "slli";
+    public static final byte OPCODE = 0b0010011;
+    public static final byte FUNCT_3 = 0x1;
+
+    public Slli(InstructionIParams data) {
+        super(new InstructionIData(OPCODE, data.rd(), FUNCT_3, data.rs1(), data.imm()));
+    }
+
+    private void exec(IRegisterFile<Register32> registerFile) {
+        try {
+            registerFile.getRegisterByNumber(rd).setValue(registerFile.getRegisterByNumber(rs1).getValue() << imm);
+        } catch (UnknownRegisterException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void link(long address) {
+        imm = (short) (address & 0b1111_1111_1111);
+    }
+
+    @Override
+    public LinkRequest getLinkRequest() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public static class Handler extends RiscV32InstructionHandler<Slli> {
+        @Override
+        public void handle(Slli instruction) {
+            instruction.exec(registerFile);
+        }
+    }
+
+    public static class Parser extends InstructionRegexParserRegisterBase<Slli> {
+        @Override
+        public Slli parse(String line) throws CompilationException {
+            String[] split = splitArguments(line, 3, NAME);
+
+            Register32 rd = castToRegister32(parseRegister(registers, split[0]));
+            Register32 rs1 = castToRegister32(parseRegister(registers, split[1]));
+            short imm = parseShort(split[2]);
+
+            try {
+                return new Slli(new InstructionIParams((byte) rd.getNumber(), (byte) rs1.getNumber(), imm));
+            } catch (IllegalArgumentException e) {
+                throw new ImmediateTooLargeException(imm);
+            }
+        }
+    }
+}

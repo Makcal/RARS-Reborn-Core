bb51848023d526e12f33be5012dcb989e86c9f63
Elina Kuzmichyova <e.kuzmichyova@innopolis.university>
2024-07-11 17:40:47 +0000

    feat: add `jalr` instruction

---
+++ src/rarsreborn/core/core/instruction/riscv/instructions/rv32i/Jalr.java
@@ -0,0 +1,58 @@
+package rarsreborn.core.core.instruction.riscv.instructions.rv32i;
+
+import rarsreborn.core.compilation.compiler.riscv.InstructionRegexParserRegisterBase;
+import rarsreborn.core.core.instruction.riscv.RiscV32InstructionHandler;
+import rarsreborn.core.core.instruction.riscv.formats.InstructionI;
+import rarsreborn.core.core.register.IRegisterFile;
+import rarsreborn.core.core.register.Register32;
+import rarsreborn.core.exceptions.compilation.CompilationException;
+import rarsreborn.core.exceptions.compilation.ImmediateTooLargeException;
+import rarsreborn.core.exceptions.compilation.UnknownRegisterException;
+
+public class Jalr extends InstructionI {
+    public static final String NAME = "jalr";
+    public static final byte OPCODE = 0b0010011;
+    public static final byte FUNCT_3 = 0x7;
+
+    public Jalr(InstructionIParams data) {
+        super(new InstructionIData(OPCODE, data.rd(), FUNCT_3, data.rs1(), data.imm()));
+    }
+
+    protected void exec(IRegisterFile<Register32> registerFile, Register32 programCounter) {
+        try {
+            registerFile.getRegisterByNumber(rd).setValue(programCounter.getValue());
+            registerFile.getRegisterByNumber(rs1).setValue(programCounter.getValue() + asNegative(imm, 21));
+        } catch (UnknownRegisterException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public static class Handler extends RiscV32InstructionHandler<Jal> {
+        @Override
+        public void handle(Jal instruction) {
+            instruction.exec(registerFile, programCounter);
+        }
+    }
+
+    public static class Parser extends InstructionRegexParserRegisterBase<Jalr> {
+        @Override
+        public Jalr parse(String line) throws CompilationException {
+            String[] split = splitArguments(line, 3, NAME);
+
+            Register32 rd = castToRegister32(parseRegister(registers, split[0]));
+            Register32 rs1 = castToRegister32(parseRegister(registers, split[1]));
+            short imm = (short) truncateNegative(parseShort(split[2]), 12);
+
+            try {
+                return new Jalr(new InstructionIParams((byte) rd.getNumber(), (byte) rs1.getNumber(), imm));
+            } catch (IllegalArgumentException e) {
+                throw new ImmediateTooLargeException(imm);
+            }
+        }
+    }
+}

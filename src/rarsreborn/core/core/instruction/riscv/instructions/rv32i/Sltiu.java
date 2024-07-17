9a5928ade40e6ce10bb107f650c7cacf75542594
Elina Kuzmichyova <e.kuzmichyova@innopolis.university>
2024-07-17 19:48:42 +0000

    feat: add `sltiu` instruction

---
+++ src/rarsreborn/core/core/instruction/riscv/instructions/rv32i/Sltiu.java
@@ -0,0 +1,58 @@
+package rarsreborn.core.core.instruction.riscv.instructions.rv32i;
+
+import rarsreborn.core.compilation.compiler.riscv.InstructionRegexParserRegisterBase;
+import rarsreborn.core.core.instruction.riscv.RiscV32InstructionHandler;
+import rarsreborn.core.core.instruction.riscv.formats.InstructionI;
+import rarsreborn.core.core.register.IRegisterFile;
+import rarsreborn.core.core.register.Register32;
+import rarsreborn.core.exceptions.compilation.CompilationException;
+import rarsreborn.core.exceptions.compilation.ImmediateTooLargeException;
+import rarsreborn.core.exceptions.execution.IllegalRegisterException;
+
+public class Sltiu extends InstructionI {
+    public static final String NAME = "sltiu";
+    public static final byte OPCODE = 0b0010011;
+    public static final byte FUNCT_3 = 0x3;
+
+    public Sltiu(InstructionIParams data) {
+        super(new InstructionIData(OPCODE, data.rd(), FUNCT_3, data.rs1(), data.imm()));
+    }
+
+    private void exec(IRegisterFile<Register32> registerFile) throws IllegalRegisterException {
+        if(registerFile.getRegisterByNumber(rs1).getValue() < imm) {
+            registerFile.getRegisterByNumber(rd).setValue(1);
+        }
+        else {
+            registerFile.getRegisterByNumber(rd).setValue(0);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public static class Handler extends RiscV32InstructionHandler<Sltiu> {
+        @Override
+        public void handle(Sltiu instruction) throws IllegalRegisterException {
+            instruction.exec(registerFile);
+        }
+    }
+
+    public static class Parser extends InstructionRegexParserRegisterBase<Sltiu> {
+        @Override
+        public Sltiu parse(String line) throws CompilationException {
+            String[] split = splitArguments(line, 3, NAME);
+
+            Register32 rd = castToRegister32(parseRegister(registers, split[0]));
+            Register32 rs1 = castToRegister32(parseRegister(registers, split[1]));
+            short imm = (short) truncateNegative(parseShort(split[2]), 12);
+
+            try {
+                return new Sltiu(new InstructionIParams((byte) rd.getNumber(), (byte) rs1.getNumber(), imm));
+            } catch (IllegalArgumentException e) {
+                throw new ImmediateTooLargeException(imm);
+            }
+        }
+    }
+}

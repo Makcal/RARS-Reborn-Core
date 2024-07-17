3b8a0107a067f8bd2f1cbc45adc46410d878137e
Elina Kuzmichyova <e.kuzmichyova@innopolis.university>
2024-07-17 23:54:34 +0000

    feat: added 'mulh' instruction

---
+++ src/rarsreborn/core/core/instruction/riscv/instructions/rv32m/Mulh.java
@@ -0,0 +1,57 @@
+package rarsreborn.core.core.instruction.riscv.instructions.rv32m;
+
+import rarsreborn.core.compilation.compiler.riscv.InstructionRegexParserRegisterBase;
+import rarsreborn.core.core.instruction.riscv.RiscV32InstructionHandler;
+import rarsreborn.core.core.instruction.riscv.formats.InstructionR;
+import rarsreborn.core.core.register.IRegisterFile;
+import rarsreborn.core.exceptions.compilation.*;
+import rarsreborn.core.core.register.Register32;
+import rarsreborn.core.exceptions.execution.IllegalRegisterException;
+
+public class Mulh extends InstructionR {
+    public static final String NAME = "mulh";
+    public static final byte OPCODE = 0b0110011;
+    public static final byte FUNCT_3 = 0x1;
+    public static final byte FUNCT_7 = 0x01;
+
+    public Mulh(InstructionRParams data) {
+        super(new InstructionRData(OPCODE, data.rd(), FUNCT_3, data.rs1(), data.rs2(), FUNCT_7));
+    }
+
+    private void exec(IRegisterFile<Register32> registerFile) throws IllegalRegisterException {
+        String gav = (Integer.toBinaryString(registerFile.getRegisterByNumber(rs1).getValue()
+        * registerFile.getRegisterByNumber(rs2).getValue())).substring(0, 32);
+        registerFile.getRegisterByNumber(rd).setValue(Integer.parseInt(gav));
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public static class Handler extends RiscV32InstructionHandler<Mulh> {
+        @Override
+        public void handle(Mulh instruction) throws IllegalRegisterException {
+            instruction.exec(registerFile);
+        }
+    }
+
+    public static class Parser extends InstructionRegexParserRegisterBase<Mulh> {
+        @Override
+        public Mulh parse(String line) throws CompilationException {
+            String[] split = splitArguments(line, 3, NAME);
+
+            Register32 rd = castToRegister32(parseRegister(registers, split[0]));
+            Register32 rs1 = castToRegister32(parseRegister(registers, split[1]));
+            Register32 rs2 = castToRegister32(parseRegister(registers, split[2]));
+
+            return new Mulh(
+                new InstructionRParams(
+                    (byte) rd.getNumber(),
+                    (byte) rs1.getNumber(),
+                    (byte) rs2.getNumber()
+                )
+            );
+        }
+    }
+}

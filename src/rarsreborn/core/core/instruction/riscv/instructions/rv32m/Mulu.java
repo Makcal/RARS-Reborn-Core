a0b1a485c82469080211b180d7c616519e9c855b
Maxim Fomin <m.fomin@innopolis.university>
2024-07-18 23:24:53 +0300

    feat: add unsigned instructions from RV32M

--- src/rarsreborn/core/core/instruction/riscv/instructions/rv32m/Mulu.java
+++ src/rarsreborn/core/core/instruction/riscv/instructions/rv32m/Mulu.java
@@ -4,25 +4,27 @@ import rarsreborn.core.compilation.compiler.riscv.InstructionRegexParserRegister
 import rarsreborn.core.core.instruction.riscv.RiscV32InstructionHandler;
 import rarsreborn.core.core.instruction.riscv.formats.InstructionR;
 import rarsreborn.core.core.register.IRegisterFile;
-import rarsreborn.core.exceptions.compilation.*;
 import rarsreborn.core.core.register.Register32;
+import rarsreborn.core.exceptions.compilation.CompilationException;
 import rarsreborn.core.exceptions.execution.IllegalRegisterException;
 
-public class Div extends InstructionR {
-    public static final String NAME = "div";
+/**
+ * Multiplies two zero-extended numbers. Returns higher 32 bits.
+ */
+public class Mulu extends InstructionR {
+    public static final String NAME = "mulu";
     public static final byte OPCODE = 0b0110011;
-    public static final byte FUNCT_3 = 0x04;
+    public static final byte FUNCT_3 = 0x3;
     public static final byte FUNCT_7 = 0x01;
 
-    public Div(InstructionRParams data) {
+    public Mulu(InstructionRParams data) {
         super(new InstructionRData(OPCODE, data.rd(), FUNCT_3, data.rs1(), data.rs2(), FUNCT_7));
     }
 
     private void exec(IRegisterFile<Register32> registerFile) throws IllegalRegisterException {
-        registerFile.getRegisterByNumber(rd).setValue(
-            registerFile.getRegisterByNumber(rs1).getValue()
-            / registerFile.getRegisterByNumber(rs2).getValue()
-        );
+        long val1 = Integer.toUnsignedLong(registerFile.getRegisterByNumber(rs1).getValue());
+        long val2 = Integer.toUnsignedLong(registerFile.getRegisterByNumber(rs2).getValue());
+        registerFile.getRegisterByNumber(rd).setValue((int) (val1 * val2 >> 32));
     }
 
     @Override
@@ -30,23 +32,23 @@ public class Div extends InstructionR {
         return NAME;
     }
 
-    public static class Handler extends RiscV32InstructionHandler<Div> {
+    public static class Handler extends RiscV32InstructionHandler<Mulu> {
         @Override
-        public void handle(Div instruction) throws IllegalRegisterException {
+        public void handle(Mulu instruction) throws IllegalRegisterException {
             instruction.exec(registerFile);
         }
     }
 
-    public static class Parser extends InstructionRegexParserRegisterBase<Div> {
+    public static class Parser extends InstructionRegexParserRegisterBase<Mulu> {
         @Override
-        public Div parse(String line) throws CompilationException {
+        public Mulu parse(String line) throws CompilationException {
             String[] split = splitArguments(line, 3, NAME);
 
             Register32 rd = castToRegister32(parseRegister(registers, split[0]));
             Register32 rs1 = castToRegister32(parseRegister(registers, split[1]));
             Register32 rs2 = castToRegister32(parseRegister(registers, split[2]));
 
-            return new Div(
+            return new Mulu(
                 new InstructionRParams((byte) rd.getNumber(), (byte) rs1.getNumber(), (byte) rs2.getNumber())
             );
         }

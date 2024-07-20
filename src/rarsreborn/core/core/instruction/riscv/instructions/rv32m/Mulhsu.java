481ab13b990d7d1f86c1b2cb0d3716c76488c94c
Maxim Fomin <m.fomin@innopolis.university>
2024-07-20 22:49:20 +0300

    fix: `mulhsu`, `mulhu` names

--- src/rarsreborn/core/core/instruction/riscv/instructions/rv32m/Mulhsu.java
+++ src/rarsreborn/core/core/instruction/riscv/instructions/rv32m/Mulhsu.java
@@ -9,20 +9,20 @@ import rarsreborn.core.exceptions.compilation.CompilationException;
 import rarsreborn.core.exceptions.execution.IllegalRegisterException;
 
 /**
- * Multiplies two zero-extended numbers. Returns higher 32 bits.
+ * Multiplies a sign-extended number (first) with a zero-extended one. Returns higher 32 bits.
  */
-public class Mulu extends InstructionR {
-    public static final String NAME = "mulu";
+public class Mulhsu extends InstructionR {
+    public static final String NAME = "mulhsu";
     public static final byte OPCODE = 0b0110011;
-    public static final byte FUNCT_3 = 0x3;
+    public static final byte FUNCT_3 = 0x2;
     public static final byte FUNCT_7 = 0x01;
 
-    public Mulu(InstructionRParams data) {
+    public Mulhsu(InstructionRParams data) {
         super(new InstructionRData(OPCODE, data.rd(), FUNCT_3, data.rs1(), data.rs2(), FUNCT_7));
     }
 
     public void exec(IRegisterFile<Register32> registerFile) throws IllegalRegisterException {
-        long val1 = Integer.toUnsignedLong(registerFile.getRegisterByNumber(rs1).getValue());
+        long val1 = registerFile.getRegisterByNumber(rs1).getValue();
         long val2 = Integer.toUnsignedLong(registerFile.getRegisterByNumber(rs2).getValue());
         registerFile.getRegisterByNumber(rd).setValue((int) (val1 * val2 >> 32));
     }
@@ -32,23 +32,23 @@ public class Mulu extends InstructionR {
         return NAME;
     }
 
-    public static class Handler extends RiscV32InstructionHandler<Mulu> {
+    public static class Handler extends RiscV32InstructionHandler<Mulhsu> {
         @Override
-        public void handle(Mulu instruction) throws IllegalRegisterException {
+        public void handle(Mulhsu instruction) throws IllegalRegisterException {
             instruction.exec(registerFile);
         }
     }
 
-    public static class Parser extends InstructionRegexParserRegisterBase<Mulu> {
+    public static class Parser extends InstructionRegexParserRegisterBase<Mulhsu> {
         @Override
-        public Mulu parse(String line) throws CompilationException {
+        public Mulhsu parse(String line) throws CompilationException {
             String[] split = splitArguments(line, 3, NAME);
 
             Register32 rd = castToRegister32(parseRegister(registers, split[0]));
             Register32 rs1 = castToRegister32(parseRegister(registers, split[1]));
             Register32 rs2 = castToRegister32(parseRegister(registers, split[2]));
 
-            return new Mulu(
+            return new Mulhsu(
                 new InstructionRParams((byte) rd.getNumber(), (byte) rs1.getNumber(), (byte) rs2.getNumber())
             );
         }
